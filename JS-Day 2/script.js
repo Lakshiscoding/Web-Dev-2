4c049185791a0110fec44895d8b210460d81335b
Laksh Vashist <student@example.com>
2026-01-28 14:45:42 +0530

    Web Dev

---
+++ JS-Day 2/script.js
@@ -0,0 +1,8 @@
+let user={
+    FullName: "Laksh",
+    Address: "Delhi",
+    MobileNo: "9910332xxx",
+    FavColour: ["Blue","Black","Red"]
+}
+
+console.log(user.FullName,user.Address,user.MobileNo,user.FavColour);

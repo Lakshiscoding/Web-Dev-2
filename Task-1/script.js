ecc70037224f001dfa9fa8b3570f3d94835b33bc
Laksh Vashist <student@example.com>
2026-02-04 14:32:48 +0530

    Web Dev

---
+++ Task-1/script.js
@@ -0,0 +1,14 @@
+const profile = document.getElementById
+("profile");
+
+profile.style.backgroundColor="tomato"
+profile.style.padding="15px"
+profile.style.textalign="center"
+
+const highlight = document.getElementByClassName("important");
+    console.log(x)
+    
+for(let i = 0 ; i<x.length;i++){
+    x[i].style.color="red"
+    x[i].style.fontsize="50px"
+}
